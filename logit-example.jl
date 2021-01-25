1e186c345322c4d7d568e58a03802ebfd01db193
Mark <mjagerton@ucdavis.edu>
2021-01-25 00:22:23 -0800

    add math to logit-example

--- logit-example.jl
+++ logit-example.jl
@@ -23,28 +23,37 @@ md"
 # Logit
 
 Suppose that agents can choose an action $y \in \{0,1\}$. The payoffs to each are
+
 ```math
 \begin{align*}
 u_0(X,\epsilon) &= \epsilon_{i0} \\
 u_1(X,\epsilon) &= x_i^\top \beta + \epsilon_{i1}
 \end{align*}
 ```
+
 The shocks $\epsilon_{i0},\epsilon_{i1}$ are distributed as iid Type-I extreme value with mean 0 and scale parameter 1. Agents choose $y_i=1$ if $u_1 \geq u_0$ and $y_0$ if $u_1 < u_0$
 
-I have simulated $X$ and $y$ below. 
+Denote $cdf(z) = F(z)$. Let choice $y_i \in \{0,1\}$.
 
-1. Write down the log likelihood function for each individual $i$, $\log L_i(y_i|X_i)$ and the score $\nabla_\beta \log L_i(y_i|X_i)$. Feel free to consult Greene
-2. Write a function to compute the log likelihood of the data: $\sum_i \log L_i(y_i|X_i)$ and the gradient of this function.
-3. Use the finite difference capabilities of `Calculus.jl` to check your gradient and make sure that it's correct
-4. Maximize the likelihood and compute standard errors using the Information Matrix. These should be
+Likelihood is
 
 ```math
-\left[\sum_i \nabla \log L_i \nabla \log L_i^\top\right]^{-1} \to Var(\beta)
+\log L(y|X) = \sum_i \log F\left( 2(y_i-1) x_i^\top \beta \right)
 ```
-    
-Greene ch 21 (5th ed) has the appropriate formulas
-    
-You might be able to make good use of the following functions from [`StatsFuns.jl`](https://github.com/JuliaStats/StatsFuns.jl)
+
+Score is
+
+```math
+\nabla_\beta \log L(y_i|x_i) = \left[y_i - F\left(x_i^\top\beta\right)\right]x_i
+```
+
+Information matrix is
+
+```math
+\left[\sum_i \nabla \log L_i \nabla \log L_i^\top\right] \to Var(\beta)
+```
+  
+We can use the following functions from [`StatsFuns.jl`](https://github.com/JuliaStats/StatsFuns.jl)
 
 ```julia
 logsumexp      # log(exp(x) + exp(y)) or log(sum(exp(x)))
@@ -57,7 +66,7 @@ seed!(1234)
 
 # ╔═╡ b9a85250-5e9b-11eb-1787-6b671b235984
 begin
-	nobs = 10_000
+	nobs = 1_000
 	β = [1.0, -2.0, 1.0, 0.5]
 	k = length(β)
 	X = randn(nobs, k);
@@ -125,13 +134,6 @@ end
 # ╔═╡ 90b963b0-5e9c-11eb-0dea-81d683f42601
 dloglik(y, X, theta) = dloglik!(similar(theta), y, X, theta)
 
-# ╔═╡ 640b3fa2-5e9c-11eb-19d3-97ebb410851a
-# closures wrap likelihood & gradient
-begin
-	f(thet) = loglik(y,X,thet)
-	g!(grad,thet) = dloglik!(grad,y,X,thet)
-end
-
 # ╔═╡ 90c65c02-5e9c-11eb-032a-7d600b60b9c4
 function informationmatrix(y, X, theta)
 
@@ -139,12 +141,19 @@ function informationmatrix(y, X, theta)
     infomatrix = zeros(k,k)
 
     u1 = X*theta
-    f(z) = cdf(Logistic(), z)    
-    g = y .- f.(u1)   # as per Greene 6th ed p. 779
+    ff(z) = cdf(Logistic(), z)    
+    g = y .- ff.(u1)   # as per Greene 6th ed p. 779
     
     infomatrix = (g .* X)' * (g .* X)
     
     return infomatrix # maybe flip signs?
+end
+
+# ╔═╡ 640b3fa2-5e9c-11eb-19d3-97ebb410851a
+# closures wrap likelihood & gradient
+begin
+	f(thet) = loglik(y,X,thet)
+	g!(grad,thet) = dloglik!(grad,y,X,thet)
 end
 
 # ╔═╡ ba5f02b0-5e9c-11eb-30cc-fbb3e2d4b6e2
@@ -175,7 +184,7 @@ begin
 end
 
 # ╔═╡ Cell order:
-# ╟─0ece0030-5e9d-11eb-0092-254fc6e72e83
+# ╠═0ece0030-5e9d-11eb-0092-254fc6e72e83
 # ╠═8595f030-5e9b-11eb-3bd7-ef8250cd1fab
 # ╠═b9c02010-5e9b-11eb-1e5d-9358c778cbba
 # ╠═b9a85250-5e9b-11eb-1787-6b671b235984
